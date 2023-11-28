2a8af734c3ee752f675c13243cb49f643d06b5e9
Andrew Gallagher <andrewjcg@meta.com>
2023-11-28 06:35:49 -0800

    [third-party/py-spy] Include more context in errors
    Summary:
    This makes sure to include the error chain included in the error,
    which is useful when debugging.
    
    Test Plan: CI
    
    Reviewers: bmaurer, kunalb, wenyinfu
    
    Reviewed By: kunalb
    
    Subscribers: mzlee
    
    Differential Revision: https://phabricator.intern.facebook.com/D51531773

--- src/python_spy.rs
+++ src/python_spy.rs
@@ -115,7 +115,7 @@ impl PythonSpy {
             if retries >= max_retries {
                 return Err(err);
             }
-            info!("Failed to connect to process, retrying. Error: {}", err);
+            info!("Failed to connect to process, retrying. Error: {:?}", err);
             std::thread::sleep(std::time::Duration::from_millis(20));
         }
     }
